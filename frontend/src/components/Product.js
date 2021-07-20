7c84c28b4143c0c77ae0d72c0f4923d83dd35f9d
eswarasurya <eswarasurya1110@gmail.com>
2021-07-20 15:47:05 +0530

    Completed home and product pages

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 function Product({_id, image, rating, numReviews, price, name}) {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${_id}`} >
+            <Link to={`/product/${_id}`} >
                 <Card.Img src={image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${_id}`}>
+                <Link to={`/product/${_id}`}>
                     <Card.Title as='div'><strong>{name}</strong></Card.Title>
-                </a>
+                </Link>
                 <Card.Text as='div'>
-                    <div className="my-3">
-                        {rating} from {numReviews} reviews
-                    </div>
+                    <Rating value={rating} text={`${numReviews} reviews`} />
                 </Card.Text>
                 <Card.Text as='h3'>${price}</Card.Text>
             </Card.Body>
