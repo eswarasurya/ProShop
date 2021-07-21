83a29f014ab8a9d663826b77cd467b582236b909
eswarasurya <eswarasurya1110@gmail.com>
2021-07-21 13:04:35 +0530

    added database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const products = require('./data/products')
+const connectDB = require('./config/db')
+
+const {notFound, errorHandler} = require('./middleware/errorMiddleware')
+const productRoutes = require('./routes/productRoutes')
 
 dotenv.config()
+
+connectDB()
 const app = express()
 
 
@@ -10,14 +15,10 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const  PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
